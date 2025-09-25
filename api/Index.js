de31569ea4edbe668d7e332de344bc09912eb0a8
SherazHashmi1111 <sherazhashmi111@gmail.com>
2025-09-25 10:35:44 +0500

    Mongodb connection established

--- api/Index.js
+++ api/Index.js
@@ -2,15 +2,28 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import mongoose from "mongoose";
 const app = express();
+dotenv.config();
+
 app.use(cookieParser());
 app.use(express.json());
 
+const FRONTEND_URL = process.env.FRONTEND_URL;
+const MONGODB_URL = process.env.MONGODB_URL;
+
 // Basic setting
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
-dotenv.config();
-const PORT = process.env.PORT ;
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(MONGODB_URL, { dbName: "mern-blog-clone" })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // Port setting
 app.listen(PORT, () => {
